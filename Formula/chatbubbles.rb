52306e9722c66584d6aa6dfda4102b08858ce11e
Kacy Fortner <kacy@kacyfortner.com>
2026-04-05 19:59:26 -0400

    fix: align formula with brew style

--- Formula/chatbubbles.rb
+++ Formula/chatbubbles.rb
@@ -1,17 +1,15 @@
 class Chatbubbles < Formula
-  desc "small https api for iMessage over tailscale"
+  desc "Small HTTPS API for iMessage over Tailscale"
   homepage "https://github.com/kacy/chatbubbles"
   version "0.1.3"
 
-  depends_on macos: :sonoma
   depends_on "imsg"
+  depends_on macos: :sonoma
 
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/kacy/chatbubbles/releases/download/v0.1.3/chatbubbles_0.1.3_darwin_arm64.tar.gz"
     sha256 "96566e1aa61e11f28a98f0075a9cbe58a1b560f2391582def5d8afd9626621a1"
-  end
-
-  on_intel do
+  else
     url "https://github.com/kacy/chatbubbles/releases/download/v0.1.3/chatbubbles_0.1.3_darwin_amd64.tar.gz"
     sha256 "a4465e28ffb956a6bf05bd6ca9d731d999957ae8bfe8bc2de1c4671142fc1003"
   end
